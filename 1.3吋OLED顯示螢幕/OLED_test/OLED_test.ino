71c5dc5a3cbdf00f457faa408bdfe47cfbd719c5
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2022-05-11 09:55:18 +0800

    20220511
    新增OLED

---
+++ 1.3吋OLED顯示螢幕/OLED_test/OLED_test.ino
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void setup(void) {
+  u8g2.begin();
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+  u8g2.drawStr(0,10,"Hello world!");  // write something to the internal memory
+  u8g2.sendBuffer();          // transfer internal memory to the display
+}
+
+void loop(void) {
+  
+}
